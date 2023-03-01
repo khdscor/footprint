092af8db899184030dc85e711b0b6c122b3b19e8
khdscor <khds_cor@naver.com>
2023-03-01 10:24:06 +0900

    feat: ê스프링부트 초기 ìí세팅

---
+++ backend/footprint/src/main/java/foot/footprint/FootprintApplication.java
@@ -0,0 +1,13 @@
+package foot.footprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootprintApplication.class, args);
+	}
+
+}
---
+++ backend/footprint/src/test/java/foot/footprint/FootprintApplicationTests.java
@@ -0,0 +1,13 @@
+package foot.footprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
