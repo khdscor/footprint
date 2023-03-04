61af4fad7d916501982af3ae74dd71ffcafe79f3
khdscor <khds_cor@naver.com>
2023-03-04 19:18:56 +0900

    feat: FindArticleController 구현 및 테스트

---
+++ backend/footprint/src/main/java/foot/footprint/dto/article/ArticleRangeRequest.java
@@ -0,0 +1,19 @@
+package foot.footprint.dto.article;
+
+import lombok.Getter;
+
+@Getter
+public class ArticleRangeRequest {
+
+    private final Double latitude;
+    private final Double latitudeRange;
+    private final Double longitude;
+    private final Double longitudeRange;
+
+    public ArticleRangeRequest(Double latitude, Double latitudeRange, Double longitude, Double longitudeRange) {
+        this.latitude = latitude;
+        this.latitudeRange = latitudeRange;
+        this.longitude = longitude;
+        this.longitudeRange = longitudeRange;
+    }
+}
