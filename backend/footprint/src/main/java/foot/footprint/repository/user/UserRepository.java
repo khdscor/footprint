24479fa6f7355ca93d3d3efbfa2fbded6eca7d9d
khdscor <khds_cor@naver.com>
2023-03-15 23:58:18 +0900

    feat: findUser 구현 및 테스트

--- backend/footprint/src/main/java/foot/footprint/repository/user/UserRepository.java
+++ backend/footprint/src/main/java/foot/footprint/repository/user/UserRepository.java
@@ -3,14 +3,16 @@ package foot.footprint.repository.user;
 import foot.footprint.domain.user.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
-
-import java.util.Date;
 
 @Mapper
 @Repository
-public interface SaveUserRepository {
+public interface UserRepository {
     @Insert("INSERT INTO user(email, image_url, join_date, nick_name, password, provider, provider_id, role) " +
             "VALUES (#{email}, #{image_url}, #{join_date}, #{nick_name}, #{password}, #{provider}, #{provider_id}, #{role})")
-    void saveUser(User user);
+    int saveUser(User user);
+
+    @Select("SELECT * FROM user WHERE id=#{id}")
+    User findUser(Long id);
 }
