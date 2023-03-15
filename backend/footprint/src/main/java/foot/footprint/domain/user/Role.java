a9db38d79da9258794f15f7e1dc49f016f8cc0f4
khdscor <khds_cor@naver.com>
2023-03-15 23:54:43 +0900

    feat:  Custom TypeHandler 구현

--- backend/footprint/src/main/java/foot/footprint/domain/user/Role.java
+++ backend/footprint/src/main/java/foot/footprint/domain/user/Role.java
@@ -1,12 +1,28 @@
 package foot.footprint.domain.user;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
-public enum Role {
+import foot.footprint.domain.typeHandler.CodeEnum;
+import foot.footprint.domain.typeHandler.CodeEnumTypeHandler;
+import org.apache.ibatis.type.MappedTypes;
+
+public enum Role implements CodeEnum {
     USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
 
-    private final String key;
+    private String code;
+
+    Role(String code) {
+        this.code = code;
+    }
+
+    @MappedTypes(Role.class)
+    public static class TypeHandler extends CodeEnumTypeHandler<Role> {
+        public TypeHandler() {
+            super(Role.class);
+        }
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
 }
