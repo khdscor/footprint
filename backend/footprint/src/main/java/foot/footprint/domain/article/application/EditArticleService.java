7a55f7da1064b7d76dfaa3861bdfbddfb45c3487
khdscor <khds_cor@naver.com>
2023-04-10 21:55:44 +0900

    feat:editArticleController 구현 및 테스트

--- backend/footprint/src/main/java/foot/footprint/domain/article/application/EditArticleService.java
+++ backend/footprint/src/main/java/foot/footprint/domain/article/application/EditArticleService.java
@@ -6,7 +6,6 @@ import foot.footprint.domain.article.domain.Article;
 import foot.footprint.domain.article.exception.NotMatchMemberException;
 import foot.footprint.global.error.exception.NotExistsException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
---
+++ backend/footprint/src/main/java/foot/footprint/domain/article/dto/EditArticleContentRequest.java
@@ -0,0 +1,17 @@
+package foot.footprint.domain.article.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+public class EditArticleContentRequest {
+
+  private String newContent;
+
+  public EditArticleContentRequest(String newContent) {
+    this.newContent = newContent;
+  }
+
+  public EditArticleContentRequest() {
+  }
+}
