1028e78cc190cfb527f80735327331574733f030
khdscor <khds_cor@naver.com>
2023-04-16 15:18:59 +0900

    refactor: @valid를 통한 회원가입, 로그인시 검증 구현

--- backend/footprint/src/main/java/foot/footprint/domain/member/dto/LoginRequest.java
+++ backend/footprint/src/main/java/foot/footprint/domain/member/dto/LoginRequest.java
@@ -3,6 +3,7 @@ package foot.footprint.domain.member.dto;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,7 +13,9 @@ public class LoginRequest {
 
   @Email
   @NotBlank
+  @Size(min = 4, max = 30)
   private String email;
   @NotBlank
+  @Size(min = 4, max = 30)
   private String password;
 }
--- backend/footprint/src/main/java/foot/footprint/domain/member/dto/SignUpRequest.java
+++ backend/footprint/src/main/java/foot/footprint/domain/member/dto/SignUpRequest.java
@@ -2,6 +2,7 @@ package foot.footprint.domain.member.dto;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,10 +11,13 @@ import lombok.Getter;
 public class SignUpRequest {
 
   @NotBlank
+  @Size(min = 4, max = 15)
   private String nickName;
   @Email
   @NotBlank
+  @Size(min = 4, max = 30)
   private String email;
   @NotBlank
+  @Size(min = 4, max = 30)
   private String password;
 }
