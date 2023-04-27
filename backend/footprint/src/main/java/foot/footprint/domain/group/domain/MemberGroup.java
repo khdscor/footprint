bc2069e55380b222072cc765f295e29496ecf8b8
khdscor <khds_cor@naver.com>
2023-04-27 22:02:42 +0900

    feat: MemberGroup 도메인 구현

--- backend/footprint/src/main/java/foot/footprint/domain/group/domain/MemberGroup.java
+++ backend/footprint/src/main/java/foot/footprint/domain/group/domain/MemberGroup.java
@@ -1,6 +1,7 @@
 package foot.footprint.domain.group.domain;
 
 import java.util.Date;
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,11 +17,21 @@ public class MemberGroup {
   private Date create_date;
   private Long group_id;
   private Long member_id;
+  private boolean important;
 
-  public MemberGroup(Long id, Date create_date, Long group_id, Long member_id) {
+  public MemberGroup(Long id, Date create_date, Long group_id, Long member_id, boolean important) {
     this.id = id;
     this.create_date = create_date;
     this.group_id = group_id;
     this.member_id = member_id;
+    this.important = important;
+  }
+
+  public void changeImportant() {
+    if (Objects.isNull(important)) {
+      important = true;
+    } else {
+      important = !important;
+    }
   }
 }
