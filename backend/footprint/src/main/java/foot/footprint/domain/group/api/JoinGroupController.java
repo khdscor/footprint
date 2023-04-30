9503f7371a5cfb954f400dce8eaf61bf71fd1959
khdscor <khds_cor@naver.com>
2023-04-30 19:14:17 +0900

    feat: JoinGroupController 구현 및 테스트

---
+++ backend/footprint/src/main/java/foot/footprint/domain/group/api/JoinGroupController.java
@@ -0,0 +1,29 @@
+package foot.footprint.domain.group.api;
+
+import foot.footprint.domain.group.application.JoinGroupService;
+import foot.footprint.global.security.user.CustomUserDetails;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/groups")
+@RequiredArgsConstructor
+public class JoinGroupController {
+
+  private final JoinGroupService joinGroupService;
+
+  @PostMapping("/join")
+  public ResponseEntity<Void> join(@RequestParam(value = "invitation_code") String invitationCode,
+      @AuthenticationPrincipal CustomUserDetails userDetails) {
+    Long groupId = joinGroupService.join(invitationCode, userDetails.getId());
+
+    return ResponseEntity.created(URI.create("/groups/" + groupId))
+        .build();
+  }
+}
