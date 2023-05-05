f3919560b80ba7e51f40941ef02530a97434d473
khdscor <khds_cor@naver.com>
2023-05-05 19:07:48 +0900

    feat: FindArticleController 수정 및 테스트

--- backend/footprint/src/main/java/foot/footprint/domain/article/api/FindArticleController.java
+++ backend/footprint/src/main/java/foot/footprint/domain/article/api/FindArticleController.java
@@ -4,10 +4,13 @@ import foot.footprint.domain.article.domain.LocationRange;
 import foot.footprint.domain.article.dto.ArticleMapResponse;
 import foot.footprint.domain.article.dto.ArticleRangeRequest;
 import foot.footprint.domain.article.application.FindArticleService;
+import foot.footprint.global.security.user.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -24,7 +27,25 @@ public class FindArticleController {
     LocationRange locationRange = new LocationRange(request);
     List<ArticleMapResponse> publicMapArticles = findArticleService.findPublicMapArticles(
         locationRange);
-    return ResponseEntity.ok()
-        .body(publicMapArticles);
+    return ResponseEntity.ok().body(publicMapArticles);
+  }
+
+  @GetMapping("/private")
+  public ResponseEntity<List<ArticleMapResponse>> findPrivateMapArticles(
+      ArticleRangeRequest request, @AuthenticationPrincipal CustomUserDetails userDetails) {
+    LocationRange locationRange = new LocationRange(request);
+    List<ArticleMapResponse> privateMapArticles = findArticleService.findPrivateMapArticles(
+        userDetails.getId(), locationRange);
+    return ResponseEntity.ok().body(privateMapArticles);
+  }
+
+  @GetMapping("/grouped")
+  public ResponseEntity<List<ArticleMapResponse>> findGroupedMapArticles(
+      @RequestParam(value = "groupId") Long groupId, ArticleRangeRequest request,
+      @AuthenticationPrincipal CustomUserDetails userDetails) {
+    LocationRange locationRange = new LocationRange(request);
+    List<ArticleMapResponse> groupedMapArticles = findArticleService.findGroupedArticles(
+        userDetails.getId(), groupId, locationRange);
+    return ResponseEntity.ok().body(groupedMapArticles);
   }
 }
