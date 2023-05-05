b131476cfaf6bed4a8ac48612c0b99c337c48f6c
khdscor <khds_cor@naver.com>
2023-05-05 17:24:47 +0900

    feat: ArticleGroupRepository#saveArticleGroupList 구현 및 테스트

---
+++ backend/footprint/src/main/java/foot/footprint/domain/group/dao/ArticleGroupRepository.java
@@ -0,0 +1,11 @@
+package foot.footprint.domain.group.dao;
+
+import foot.footprint.domain.group.domain.ArticleGroup;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ArticleGroupRepository {
+
+  int saveArticleGroupList(List<ArticleGroup> articleGroups);
+}
