5ba9faa3c8466c3b1b2d7373d1faa72da4479962
khdscor <khds_cor@naver.com>
2023-05-07 15:41:44 +0900

    feat: existsArticleInMyGroup 구현 및 테스트: 게시글과 member가 같은 그룹인지 확인하는 dao 구현 및 테스트

--- backend/footprint/src/test/java/foot/footprint/repository/group/ArticleGroupRepositoryTest.java
+++ backend/footprint/src/test/java/foot/footprint/repository/group/ArticleGroupRepositoryTest.java
@@ -6,8 +6,10 @@ import foot.footprint.domain.article.dao.CreateArticleRepository;
 import foot.footprint.domain.article.domain.Article;
 import foot.footprint.domain.group.dao.ArticleGroupRepository;
 import foot.footprint.domain.group.dao.GroupRepository;
+import foot.footprint.domain.group.dao.MemberGroupRepository;
 import foot.footprint.domain.group.domain.ArticleGroup;
 import foot.footprint.domain.group.domain.Group;
+import foot.footprint.domain.group.domain.MemberGroup;
 import foot.footprint.domain.member.dao.MemberRepository;
 import foot.footprint.domain.member.domain.Member;
 import foot.footprint.repository.RepositoryTest;
@@ -30,6 +32,9 @@ public class ArticleGroupRepositoryTest extends RepositoryTest {
   @Autowired
   private ArticleGroupRepository articleGroupRepository;
 
+  @Autowired
+  private MemberGroupRepository memberGroupRepository;
+
   @Test
   public void saveArticleGroupList() {
     //given
@@ -50,6 +55,31 @@ public class ArticleGroupRepositoryTest extends RepositoryTest {
 
     //then
     assertThat(result).isEqualTo(2);
+
+  }
+
+  @Test
+  public void existsArticleInMyGroup() {
+    //given
+    Member member = buildMember();
+    memberRepository.saveMember(member);
+    Article article = buildArticle(member.getId());
+    createArticleRepository.saveArticle(article);
+    Group group = buildGroup(member.getId());
+    groupRepository.saveGroup(group);
+    List<ArticleGroup> articleGroups = new ArrayList<>();
+    articleGroups.add(buildArticleGroup(group.getId(), article.getId()));
+    articleGroupRepository.saveArticleGroupList(articleGroups);
+    MemberGroup memberGroup = MemberGroup.createMemberGroup(group);
+    memberGroupRepository.saveMemberGroup(memberGroup);
+
+    //when & then
+    assertThat(
+        articleGroupRepository.existsArticleInMyGroup(article.getId(), member.getId())).isTrue();
+    assertThat(
+        articleGroupRepository.existsArticleInMyGroup(100L, member.getId())).isFalse();
+    assertThat(
+        articleGroupRepository.existsArticleInMyGroup(article.getId(), 100L)).isFalse();
 
   }
 
