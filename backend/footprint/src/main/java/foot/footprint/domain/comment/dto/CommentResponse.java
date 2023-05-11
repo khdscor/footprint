8a121c0072c6f279006d15909471285c0891539d
khdscor <khds_cor@naver.com>
2023-05-11 21:24:59 +0900

    feat: 댓글 작성 후 댓글 갱신 위해 리턴 dto 구현

---
+++ backend/footprint/src/main/java/foot/footprint/domain/comment/dto/CommentResponse.java
@@ -0,0 +1,40 @@
+package foot.footprint.domain.comment.dto;
+
+import foot.footprint.domain.comment.domain.Comment;
+import java.util.Date;
+import lombok.Getter;
+
+@Getter
+public class CommentResponse {
+
+  private Long id;
+  private String content;
+  private AuthorDto author;
+  private Date createDate;
+  private Long totalLikes;
+
+  public CommentResponse(Long id, String content, AuthorDto author, Date createDate) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+    this.createDate = createDate;
+  }
+
+  public CommentResponse(Long id, String content, Long userId, String nickName, String imageUrl,
+      Date createDate, Long totalLikes) {
+    this.id = id;
+    this.content = content;
+    this.author = new AuthorDto(userId, nickName, imageUrl);
+    this.createDate = createDate;
+    this.totalLikes = totalLikes;
+  }
+
+  public static CommentResponse toCommentResponse(Comment comment, AuthorDto authorDto) {
+    return new CommentResponse(
+        comment.getId(),
+        comment.getContent(),
+        authorDto,
+        comment.getCreate_date()
+    );
+  }
+}
