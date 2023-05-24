0a5b846a74c4e69c0411aee9f3a2d3e02b26c0c4
khdscor <khds_cor@naver.com>
2023-05-24 22:19:34 +0900

    refactor: articleLike 그룹 게시글일 경우 추가 테스트

--- backend/footprint/src/test/java/foot/footprint/service/articleLike/ArticleLikeServiceTest.java
+++ backend/footprint/src/test/java/foot/footprint/service/articleLike/ArticleLikeServiceTest.java
@@ -12,6 +12,8 @@ import foot.footprint.domain.article.exception.NotMatchMemberException;
 import foot.footprint.domain.articleLike.application.ArticleLikeService;
 import foot.footprint.domain.articleLike.dao.ArticleLikeRepository;
 import foot.footprint.domain.articleLike.dto.ArticleLikeDto;
+import foot.footprint.domain.group.dao.ArticleGroupRepository;
+import foot.footprint.global.error.exception.NotAuthorizedOrExistException;
 import foot.footprint.global.error.exception.NotExistsException;
 import java.util.Date;
 import java.util.Optional;
@@ -31,6 +33,10 @@ public class ArticleLikeServiceTest {
 
   @Mock
   private FindArticleRepository findArticleRepository;
+
+  @Mock
+  private ArticleGroupRepository articleGroupRepository;
+
   @Spy
   @InjectMocks
   private ArticleLikeService articleLikeService;
@@ -41,7 +47,7 @@ public class ArticleLikeServiceTest {
     //given
     Long articleId = 1L;
     Long memberId = 1L;
-    Article article = buildArticle(memberId, true);
+    Article article = buildArticle(memberId, true, true);
     ArticleLikeDto likedArticle = new ArticleLikeDto(articleId, memberId, true);
     ArticleLikeDto notLikedArticle = new ArticleLikeDto(articleId, memberId, false);
     given(articleLikeRepository.deleteArticleLike(any()))
@@ -80,7 +86,7 @@ public class ArticleLikeServiceTest {
     Long articleId = 1L;
     Long memberId = 1L;
     Long anotherMemberId = 2L;
-    Article article = buildArticle(memberId, false);
+    Article article = buildArticle(memberId, false, true);
     ArticleLikeDto articleLikeDto = new ArticleLikeDto(articleId, anotherMemberId);
     given(findArticleRepository.findById(any()))
         .willReturn(Optional.ofNullable(article));
@@ -91,13 +97,34 @@ public class ArticleLikeServiceTest {
         .isInstanceOf(NotMatchMemberException.class);
   }
 
-  private Article buildArticle(Long memberId, boolean publicMap) {
+  @Test
+  @DisplayName("그룹 확인 가능 게시글 좋아요 변화")
+  public void changeArticleLike_IfGroupedArticle() {
+    //changeLike에 대한 테스트는 publicArticle에서 진행하였으므로 validate만 진행
+    //given
+    Long articleId = 1L;
+    Long memberId = 1L;
+    Long anotherMemberId = 2L;
+    Article article = buildArticle(memberId, false, false);
+    ArticleLikeDto articleLikeDto = new ArticleLikeDto(articleId, anotherMemberId);
+    given(findArticleRepository.findById(any()))
+        .willReturn(Optional.ofNullable(article));
+    given(articleGroupRepository.existsArticleInMyGroup(any(), any()))
+        .willReturn(false);
+
+    //when & then
+    assertThatThrownBy(
+        () -> articleLikeService.changeArticleLike(articleLikeDto))
+        .isInstanceOf(NotAuthorizedOrExistException.class);
+  }
+
+  private Article buildArticle(Long memberId, boolean publicMap, boolean privateMap) {
     return Article.builder()
         .content("test")
         .latitude(10.0)
         .longitude(10.0)
         .public_map(publicMap)
-        .private_map(true)
+        .private_map(privateMap)
         .title("test")
         .create_date(new Date())
         .member_id(memberId).build();
