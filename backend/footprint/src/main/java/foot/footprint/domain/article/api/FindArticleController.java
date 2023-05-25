bb0f8d44624c5f50226496a06326cbd1dbc7405f
khdscor <khds_cor@naver.com>
2023-05-25 21:33:45 +0900

    refactor: findArticle Request 예외 validate 추가

--- backend/footprint/src/main/java/foot/footprint/domain/article/api/FindArticleController.java
+++ backend/footprint/src/main/java/foot/footprint/domain/article/api/FindArticleController.java
@@ -4,6 +4,7 @@ import foot.footprint.domain.article.domain.LocationRange;
 import foot.footprint.domain.article.dto.ArticleMapResponse;
 import foot.footprint.domain.article.dto.ArticleRangeRequest;
 import foot.footprint.domain.article.application.FindArticleService;
+import foot.footprint.global.error.exception.WrongInputException;
 import foot.footprint.global.security.user.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,7 @@ public class FindArticleController {
   @GetMapping("/public")
   public ResponseEntity<List<ArticleMapResponse>> findPublicMapArticles(
       ArticleRangeRequest request) {
+    validateLocation(request);
     LocationRange locationRange = new LocationRange(request);
     List<ArticleMapResponse> publicMapArticles = findArticleService.findPublicMapArticles(
         locationRange);
@@ -33,6 +35,7 @@ public class FindArticleController {
   @GetMapping("/private")
   public ResponseEntity<List<ArticleMapResponse>> findPrivateMapArticles(
       ArticleRangeRequest request, @AuthenticationPrincipal CustomUserDetails userDetails) {
+    validateLocation(request);
     LocationRange locationRange = new LocationRange(request);
     List<ArticleMapResponse> privateMapArticles = findArticleService.findPrivateMapArticles(
         userDetails.getId(), locationRange);
@@ -43,9 +46,17 @@ public class FindArticleController {
   public ResponseEntity<List<ArticleMapResponse>> findGroupedMapArticles(
       @RequestParam(value = "groupId") Long groupId, ArticleRangeRequest request,
       @AuthenticationPrincipal CustomUserDetails userDetails) {
+    validateLocation(request);
     LocationRange locationRange = new LocationRange(request);
     List<ArticleMapResponse> groupedMapArticles = findArticleService.findGroupedArticles(
         userDetails.getId(), groupId, locationRange);
     return ResponseEntity.ok().body(groupedMapArticles);
+  }
+
+  private void validateLocation(ArticleRangeRequest request) {
+    if (request.getLatitude() >= 180 || request.getLatitude() <= -180
+        || request.getLongitude() >= 180 || request.getLongitude() <= -180) {
+      throw new WrongInputException("위치 좌표가 범위를 넘었습니다.");
+    }
   }
 }
