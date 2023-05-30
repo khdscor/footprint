af0143eefaf87cea73e7f83fda56cb3de71b3bc7
khdscor <khds_cor@naver.com>
2023-05-30 20:57:57 +0900

    feat:EditCommentController 구현 및 테스트

---
+++ backend/footprint/src/main/java/foot/footprint/domain/comment/api/EditCommentController.java
@@ -0,0 +1,33 @@
+package foot.footprint.domain.comment.api;
+
+
+import foot.footprint.domain.comment.application.EditCommentService;
+import foot.footprint.domain.comment.dto.CommentResponse;
+import foot.footprint.domain.comment.dto.EditCommentRequest;
+import foot.footprint.global.security.user.CustomUserDetails;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+public class EditCommentController {
+
+  private final EditCommentService editCommentService;
+  @PutMapping("/{commentId}")
+  public ResponseEntity<CommentResponse> editComment(
+      @PathVariable("commentId") Long commentId,
+      @RequestBody @Valid EditCommentRequest editCommentRequest,
+      @AuthenticationPrincipal CustomUserDetails userDetails) {
+    editCommentService.edit(commentId, userDetails.getId(), editCommentRequest.getContent());
+    return ResponseEntity.noContent()
+        .build();
+  }
+}
