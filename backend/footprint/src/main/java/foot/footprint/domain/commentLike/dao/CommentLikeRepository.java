5e6874b4916e2f7bbcf9d9ad0e45c20c75cce6ca
khdscor <khds_cor@naver.com>
2023-06-02 22:50:33 +0900

    feat: CommentLikeRepository#findCommentIdsILiked 구현 및 테스트

--- backend/footprint/src/main/java/foot/footprint/domain/commentLike/dao/CommentLikeRepository.java
+++ backend/footprint/src/main/java/foot/footprint/domain/commentLike/dao/CommentLikeRepository.java
@@ -1,10 +1,13 @@
 package foot.footprint.domain.commentLike.dao;
 
 import foot.footprint.domain.commentLike.domain.CommentLike;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CommentLikeRepository {
 
   int saveCommentLike(CommentLike commentLike);
+
+  List<Long> findCommentIdsILiked(Long articleId, Long memberId);
 }
--- backend/footprint/src/test/java/foot/footprint/repository/commentLike/CommentLIkeRepositoryTest.java
+++ backend/footprint/src/test/java/foot/footprint/repository/commentLike/CommentLIkeRepositoryTest.java
@@ -11,6 +11,7 @@ import foot.footprint.domain.commentLike.domain.CommentLike;
 import foot.footprint.domain.member.dao.MemberRepository;
 import foot.footprint.domain.member.domain.Member;
 import foot.footprint.repository.RepositoryTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -43,6 +44,42 @@ public class CommentLIkeRepositoryTest extends RepositoryTest {
 
     //then
     assertThat(commentLike.getId()).isNotNull();
+  }
+
+  @Test
+  public void findHasILiked() {
+    //given
+    // 2명의 유저
+    Member member = buildMember();
+    memberRepository.saveMember(member);
+    Member another = buildMember();
+    memberRepository.saveMember(another);
+    //게시글 생성
+    Article article = buildArticle(member.getId());
+    createArticleRepository.saveArticle(article);
+    //4개의 댓글, 3개는 member, 1개는 another
+    Comment comment1 = buildComment(member.getId(), article.getId());
+    createCommentRepository.saveComment(comment1);
+    Comment comment2 = buildComment(member.getId(), article.getId());
+    createCommentRepository.saveComment(comment2);
+    Comment comment3 = buildComment(member.getId(), article.getId());
+    createCommentRepository.saveComment(comment3);
+    Comment comment4 = buildComment(another.getId(), article.getId());
+    createCommentRepository.saveComment(comment4);
+    //3개의 좋아요, 2개는 member 1개는 another
+    CommentLike commentLike1 = createCommentLike(comment1.getId(), member.getId());
+    commentLikeRepository.saveCommentLike(commentLike1);
+    CommentLike commentLike2 = createCommentLike(comment2.getId(), another.getId());
+    commentLikeRepository.saveCommentLike(commentLike2);
+    CommentLike commentLike4 = createCommentLike(comment4.getId(), member.getId());
+    commentLikeRepository.saveCommentLike(commentLike4);
+
+    //when
+    List<Long> commentLikes = commentLikeRepository.findCommentIdsILiked(article.getId(),
+        member.getId());
+
+    //then
+    assertThat(commentLikes).hasSize(2);
   }
 
   private CommentLike createCommentLike(Long commentId, Long memberId) {
