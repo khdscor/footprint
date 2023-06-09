69a697e140ca8f8faec0608712b5bab926daab77
khdscor <khds_cor@naver.com>
2023-06-09 23:02:29 +0900

    test: 그룹원 추방 service 테스트

---
+++ backend/footprint/src/test/java/foot/footprint/service/group/DeportMemberServiceTest.java
@@ -0,0 +1,67 @@
+package foot.footprint.service.group;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import foot.footprint.domain.article.exception.NotMatchMemberException;
+import foot.footprint.domain.group.application.DeportMemberService;
+import foot.footprint.domain.group.dao.GroupRepository;
+import foot.footprint.domain.group.dao.MemberGroupRepository;
+import foot.footprint.domain.group.domain.Group;
+import foot.footprint.global.error.exception.NotExistsException;
+import java.util.Date;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class DeportMemberServiceTest {
+
+  @Mock
+  private MemberGroupRepository memberGroupRepository;
+
+  @Mock
+  private GroupRepository groupRepository;
+
+  @InjectMocks
+  private DeportMemberService deportMemberService;
+
+  @Test
+  @DisplayName("그룹원 추방 - 그룹의 주인이 아닐 시")
+  public void deport_IfGroupIsNotMine() {
+    //given
+    Long ownerId = 12L;
+    Group group = buildGroup(1000L);
+    given(groupRepository.findById(any())).willReturn(Optional.ofNullable(group));
+
+    //when & then
+    assertThatThrownBy(
+        () -> deportMemberService.deport(1L, 1L, ownerId))
+        .isInstanceOf(NotMatchMemberException.class);
+  }
+
+  @Test
+  @DisplayName("그룹원 추방 - 그룹원이 존재하지 않을 시")
+  public void deport_IfMemberNotExists() {
+    //given
+    Long ownerId = 12L;
+    Group group = buildGroup(ownerId);
+    given(groupRepository.findById(any())).willReturn(Optional.ofNullable(group));
+    given(memberGroupRepository.deleteMemberGroup(any(), any())).willReturn(0);
+
+    //when & then
+    assertThatThrownBy(
+        () -> deportMemberService.deport(1L, 1L, ownerId))
+        .isInstanceOf(NotExistsException.class);
+  }
+
+  private Group buildGroup(Long ownerId) {
+    return Group.builder().create_date(new Date()).name("test_group").invitation_code("testCode")
+        .owner_id(ownerId).build();
+  }
+}
