71edb619d76b0ddd3ea97a13250ba8633b48f81b
khdscor <khds_cor@naver.com>
2023-06-09 21:42:58 +0900

    feat: ChangeImportantController 구현 및 테스트

---
+++ backend/footprint/src/main/java/foot/footprint/domain/group/api/ChangeImportantController.java
@@ -0,0 +1,27 @@
+package foot.footprint.domain.group.api;
+
+import foot.footprint.domain.group.application.ChangeImportantService;
+import foot.footprint.global.security.user.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/groups")
+public class ChangeImportantController {
+
+  private final ChangeImportantService changeImportantService;
+
+  @PutMapping("/{groupId}/important")
+  public ResponseEntity<Void> changeImportant(@PathVariable("groupId") Long groupId,
+      @AuthenticationPrincipal CustomUserDetails userDetails) {
+    changeImportantService.changeImportant(groupId, userDetails.getId());
+
+    return ResponseEntity.noContent().build();
+  }
+}
