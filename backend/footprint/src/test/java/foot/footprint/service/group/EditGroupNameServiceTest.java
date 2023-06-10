fd3721b81a2b5be799a6999b2b05222cf2ee650b
khdscor <khds_cor@naver.com>
2023-06-10 16:30:52 +0900

    feat: EditGroupNameService 구현 및 테스트

---
+++ backend/footprint/src/test/java/foot/footprint/service/group/EditGroupNameServiceTest.java
@@ -0,0 +1,37 @@
+package foot.footprint.service.group;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import foot.footprint.domain.group.application.EditGroupNameService;
+import foot.footprint.domain.group.dao.GroupRepository;
+import foot.footprint.global.error.exception.NotAuthorizedOrExistException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EditGroupNameServiceTest {
+
+  @Mock
+  private GroupRepository groupRepository;
+
+  @InjectMocks
+  private EditGroupNameService editGroupNameService;
+
+  @Test
+  @DisplayName("예외 발생 테스트")
+  public void change() {
+    //given
+    given(groupRepository.changeGroupName(any(), any(), any())).willReturn(0);
+
+    //when & then
+    assertThatThrownBy(
+        () -> editGroupNameService.change(any(), any(), any()))
+        .isInstanceOf(NotAuthorizedOrExistException.class);
+  }
+}
