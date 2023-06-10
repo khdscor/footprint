c0304874ee31b73298d77493165f1c81de9086aa
khdscor <khds_cor@naver.com>
2023-06-10 17:16:19 +0900

    feat: GroupSummaryResponse 구현

---
+++ backend/footprint/src/main/java/foot/footprint/domain/group/dto/GroupSummaryResponse.java
@@ -0,0 +1,12 @@
+package foot.footprint.domain.group.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupSummaryResponse {
+
+  private Long id;
+  private String name;
+}
