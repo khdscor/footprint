16b9fdd47acc6084f0e57e93aa9ab1dfa22521b9
Khdscor <khds_cor@naver.com>
2023-06-16 20:20:42 +0900

    feat: service#findMyPageInfo 구현

--- backend/footprint/src/main/java/foot/footprint/domain/member/application/MemberService.java
+++ backend/footprint/src/main/java/foot/footprint/domain/member/application/MemberService.java
@@ -3,6 +3,7 @@ package foot.footprint.domain.member.application;
 import foot.footprint.domain.member.dao.MemberRepository;
 import foot.footprint.domain.member.domain.Member;
 import foot.footprint.domain.member.dto.MemberImageResponse;
+import foot.footprint.domain.member.dto.MyPageResponse;
 import foot.footprint.global.error.exception.NotExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,8 +18,12 @@ public class MemberService {
     @Transactional(readOnly = true)
     public MemberImageResponse findImageUrl(Long memberId) {
         Member member = memberRepository.findById(memberId)
-            .orElseThrow(() -> new NotExistsException(
-                "존재하지 않는 회원입니다."));
+            .orElseThrow(() -> new NotExistsException("존재하지 않는 회원입니다."));
         return new MemberImageResponse(member.getId(), member.getImage_url());
+    }
+
+    @Transactional(readOnly = true)
+    public MyPageResponse findMyPageInfo(Long id) {
+        return memberRepository.findMyPageDetails(id);
     }
 }
