e9301c0fa844ac18bc131f6e6b3816deb85390ea
Khdscor <khds_cor@naver.com>
2023-06-28 22:33:14 +0900

    feat: 회원 탈퇴 controller 메서드 구현 및 테스트

--- backend/footprint/src/main/java/foot/footprint/domain/member/api/MemberController.java
+++ backend/footprint/src/main/java/foot/footprint/domain/member/api/MemberController.java
@@ -3,10 +3,12 @@ package foot.footprint.domain.member.api;
 import foot.footprint.domain.member.application.MemberService;
 import foot.footprint.domain.member.dto.MemberImageResponse;
 import foot.footprint.domain.member.dto.MyPageResponse;
+import foot.footprint.global.aop.auth.MemberLog;
 import foot.footprint.global.security.user.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +34,14 @@ public class MemberController {
         MyPageResponse response = memberService.findMyPageInfo(userDetails.getId());
 
         return ResponseEntity.ok().body(response);
+    }
+
+    @MemberLog
+    @DeleteMapping
+    public ResponseEntity<Void> withdraw(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        memberService.withdraw(userDetails.getId());
+
+        return ResponseEntity.noContent()
+            .build();
     }
 }
