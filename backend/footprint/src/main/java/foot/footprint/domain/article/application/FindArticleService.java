294b0251f9412bccbadb6107e8250706b25a0f4a
Khdscor <khds_cor@naver.com>
2023-06-29 19:51:42 +0900

    refactor: FindArticleService#findGroupedArticles groupName도 같이 리턴하도록 수정 및 테스트

--- backend/footprint/src/main/java/foot/footprint/domain/article/application/FindArticleService.java
+++ backend/footprint/src/main/java/foot/footprint/domain/article/application/FindArticleService.java
@@ -4,7 +4,8 @@ import foot.footprint.domain.article.domain.Article;
 import foot.footprint.domain.article.dto.ArticleMapResponse;
 import foot.footprint.domain.article.dao.FindArticleRepository;
 import foot.footprint.domain.article.domain.LocationRange;
-import foot.footprint.domain.group.dao.MemberGroupRepository;
+import foot.footprint.domain.article.dto.GroupMapArticlesDto;
+import foot.footprint.domain.group.dao.GroupRepository;
 import foot.footprint.global.error.exception.NotExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,37 +18,34 @@ public class FindArticleService {
 
     private final FindArticleRepository findArticleRepository;
 
-    private final MemberGroupRepository memberGroupRepository;
+    private final GroupRepository groupRepository;
 
     @Transactional(readOnly = true)
-    public List<ArticleMapResponse> findPublicMapArticles(
-        LocationRange locationRange) {
+    public List<ArticleMapResponse> findPublicMapArticles(LocationRange locationRange) {
         Long memberId = null;
         return ArticleMapResponse.toResponses(findArticles(memberId, locationRange));
     }
 
     @Transactional(readOnly = true)
-    public List<ArticleMapResponse> findPrivateMapArticles(
-        Long memberId, LocationRange locationRange) {
+    public List<ArticleMapResponse> findPrivateMapArticles(Long memberId,
+        LocationRange locationRange) {
         return ArticleMapResponse.toResponses(findArticles(memberId, locationRange));
     }
 
     @Transactional(readOnly = true)
-    public List<ArticleMapResponse> findGroupedArticles(
-        Long memberId, Long groupId, LocationRange locationRange) {
-        validateHasJoined(groupId, memberId);
+    public GroupMapArticlesDto findGroupedArticles(Long memberId, Long groupId,
+        LocationRange locationRange) {
+        String groupName = findGroupName(groupId, memberId);
         List<Article> articles = findArticleRepository.findArticlesByGroup(groupId, locationRange);
-        return ArticleMapResponse.toResponses(articles);
+        return new GroupMapArticlesDto(ArticleMapResponse.toResponses(articles), groupName);
     }
 
     private List<Article> findArticles(Long memberId, LocationRange locationRange) {
         return findArticleRepository.findArticles(memberId, locationRange);
     }
 
-    private void validateHasJoined(Long groupId, Long memberId) {
-        boolean isPresent = memberGroupRepository.checkAlreadyJoined(groupId, memberId);
-        if (!isPresent) {
-            throw new NotExistsException("그룹에 속해있지 않습니다.");
-        }
+    private String findGroupName(Long groupId, Long memberId) {
+        return groupRepository.findGroupName(memberId, groupId)
+            .orElseThrow(() -> new NotExistsException("그룹에 속해있지 않거나 해당되는 그룹이 존재하지 않습니다."));
     }
 }
