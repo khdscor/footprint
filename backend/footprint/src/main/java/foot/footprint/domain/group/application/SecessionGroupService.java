424fb91d55f888befc5b9eb02970e3aae860a176
Khdscor <khds_cor@naver.com>
2023-06-30 19:16:14 +0900

    refactor:SecessionGroupService 리펙토링 및 테스트

--- backend/footprint/src/main/java/foot/footprint/domain/group/application/SecessionGroupService.java
+++ backend/footprint/src/main/java/foot/footprint/domain/group/application/SecessionGroupService.java
@@ -2,11 +2,17 @@ package foot.footprint.domain.group.application;
 
 import foot.footprint.domain.group.dao.GroupRepository;
 import foot.footprint.domain.group.dao.MemberGroupRepository;
+import foot.footprint.domain.group.domain.Group;
+import foot.footprint.domain.group.domain.MemberGroup;
 import foot.footprint.global.error.exception.NotExistsException;
+import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SecessionGroupService {
@@ -17,21 +23,47 @@ public class SecessionGroupService {
 
     @Transactional
     public void secessionGroup(Long groupId, Long memberId) {
+        Group group = findGroup(groupId);
+        if (group.getOwner_id().equals(memberId)) {
+            checkRemainingMember(groupId, memberId);
+            return;
+        }
         deleteMemberGroup(groupId, memberId);
-        deleteGroupIfMemberIsNotExist(groupId);
+    }
+
+    private Group findGroup(Long groupId) {
+        return groupRepository.findById(groupId)
+            .orElseThrow(() -> new NotExistsException("해당되는 그룹이 존재하지 않습니다."));
+    }
+
+    private void checkRemainingMember(Long groupId, Long memberId) {
+        List<MemberGroup> memberGroups = memberGroupRepository.findAllByGroupId(groupId);
+        //owner 만 그룹에 남아있을 경우
+        if (memberGroups.size() == 1) {
+            groupRepository.deleteById(groupId);
+            log.info(groupId + " 그룹이 삭제되었습니다.");
+            return;
+        }
+        updateOwner(groupId, memberId, memberGroups);
+    }
+
+    private void updateOwner(Long groupId, Long memberId, List<MemberGroup> memberGroups) {
+        MemberGroup nextMemberGroup = findNextOwner(memberGroups, memberId);
+        Long nextOwnerId = nextMemberGroup.getMember_id();
+        groupRepository.updateOwner(groupId, nextOwnerId);
+        deleteMemberGroup(groupId, memberId);
+    }
+
+    private MemberGroup findNextOwner(List<MemberGroup> memberGroups, Long memberId) {
+        return memberGroups.stream()
+            .filter(memberGroup -> !Objects.equals(memberGroup.getMember_id(), memberId))
+            .findFirst().orElseThrow(() -> new NotExistsException("해당되는 회원이 존재하지 않습니다."));
     }
 
     private void deleteMemberGroup(Long groupId, Long memberId) {
         int deleted = memberGroupRepository.deleteMemberGroup(groupId, memberId);
         if (deleted == 0) {
             throw new NotExistsException("이미 탈퇴한 그룹이거나 존재하지 않는 그룹입니다.");
-        }
-    }
-
-    private void deleteGroupIfMemberIsNotExist(Long groupId) {
-        Long memberCount = memberGroupRepository.countMemberGroup(groupId);
-        if (memberCount <= 0) {
-            groupRepository.deleteById(groupId);
         }
     }
 }
--- backend/footprint/src/test/java/foot/footprint/service/group/SecessionGroupServiceTest.java
+++ backend/footprint/src/test/java/foot/footprint/service/group/SecessionGroupServiceTest.java
@@ -1,6 +1,5 @@
 package foot.footprint.service.group;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
@@ -9,12 +8,15 @@ import static org.mockito.Mockito.verify;
 import foot.footprint.domain.group.application.SecessionGroupService;
 import foot.footprint.domain.group.dao.GroupRepository;
 import foot.footprint.domain.group.dao.MemberGroupRepository;
-import foot.footprint.global.error.exception.NotExistsException;
+import foot.footprint.domain.group.domain.Group;
+import foot.footprint.domain.group.domain.MemberGroup;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -24,56 +26,73 @@ public class SecessionGroupServiceTest {
     @Spy
     private GroupRepository groupRepository;
 
-    @Mock
+    @Spy
     private MemberGroupRepository memberGroupRepository;
 
     @InjectMocks
     private SecessionGroupService secessionGroupService;
 
     @Test
-    @DisplayName("그룹 탈퇴 - 그룹 삭제 x")
-    public void secessionGroup() {
+    @DisplayName("그룹 탈퇴 - 그룹의 주인이 아닐경우")
+    public void secessionGroupIfNotOwner() {
         //given
-        given(memberGroupRepository.deleteMemberGroup(any(), any()))
-            .willReturn(1);
-        given(memberGroupRepository.countMemberGroup(any()))
-            .willReturn(1L);
+        Long groupId = 30L;
+        Long ownerId = 1L;
+        Long accessMemberId = 2L;
+        Group group = SetUpMethods.buildGroup(ownerId);
+        given(groupRepository.findById(any())).willReturn(Optional.ofNullable(group));
+        given(memberGroupRepository.deleteMemberGroup(any(), any())).willReturn(1);
 
         //when
-        secessionGroupService.secessionGroup(1L, 1L);
+        secessionGroupService.secessionGroup(groupId, accessMemberId);
+
+        //then
+        verify(memberGroupRepository, times(0)).findAllByGroupId(any());
+    }
+
+    @Test
+    @DisplayName("그룹 탈퇴 - 그룹의 주인이고 남은 그룹원이 존재할 경우")
+    public void secessionGroupIfOwner() {
+        //given
+        Long groupId = 30L;
+        Long ownerId = 1L;
+        Long secondMemberId = 2L;
+        Group group = SetUpMethods.buildGroup(ownerId);
+        List<MemberGroup> memberGroups = new ArrayList<>();
+        memberGroups.add(SetUpMethods.buildMemberGroup(groupId, ownerId));
+        memberGroups.add(SetUpMethods.buildMemberGroup(groupId, secondMemberId));
+        given(groupRepository.findById(any())).willReturn(Optional.ofNullable(group));
+        given(memberGroupRepository.findAllByGroupId(any())).willReturn(memberGroups);
+        given(memberGroupRepository.deleteMemberGroup(any(), any())).willReturn(1);
+        given(groupRepository.updateOwner(any(), any())).willReturn(1);
+
+        //when
+        secessionGroupService.secessionGroup(groupId, ownerId);
 
         //then
         verify(groupRepository, times(0)).deleteById(any());
+        verify(groupRepository, times(1)).updateOwner(any(), any());
+        verify(memberGroupRepository, times(1)).deleteMemberGroup(any(), any());
     }
 
     @Test
-    @DisplayName("그룹 탈퇴 - 해당하는 그룹이 없거나 가입이 안되어있을 때")
-    public void secessionGroup_IfNotExistsGroup() {
+    @DisplayName("그룹 탈퇴 - 그룹의 주인이고 혼자 남았을 경우")
+    public void secessionGroupIfOwnerRemainAlone() {
         //given
-        given(memberGroupRepository.deleteMemberGroup(any(), any()))
-            .willReturn(0);
-
-        //when & then
-        assertThatThrownBy(
-            () -> secessionGroupService.secessionGroup(1L, 1L))
-            .isInstanceOf(NotExistsException.class);
-    }
-
-    @Test
-    @DisplayName("그룹 탈퇴 - 탈퇴 후 그룹에 남은 인원이 없을 시")
-    public void secessionGroup_IfMemberIsNotExists() {
-        //given
-        given(memberGroupRepository.deleteMemberGroup(any(), any()))
-            .willReturn(1);
-        given(memberGroupRepository.countMemberGroup(any()))
-            .willReturn(0L);
-        given(groupRepository.deleteById(any()))
-            .willReturn(1);
+        Long groupId = 30L;
+        Long ownerId = 1L;
+        Group group = SetUpMethods.buildGroup(ownerId);
+        List<MemberGroup> memberGroups = new ArrayList<>();
+        memberGroups.add(SetUpMethods.buildMemberGroup(groupId, ownerId));
+        given(groupRepository.findById(any())).willReturn(Optional.ofNullable(group));
+        given(memberGroupRepository.findAllByGroupId(any())).willReturn(memberGroups);
+        given(groupRepository.deleteById(any())).willReturn(1);
 
         //when
-        secessionGroupService.secessionGroup(1L, 1L);
+        secessionGroupService.secessionGroup(groupId, ownerId);
 
         //then
         verify(groupRepository, times(1)).deleteById(any());
+        verify(memberGroupRepository, times(0)).deleteMemberGroup(any(), any());
     }
 }
