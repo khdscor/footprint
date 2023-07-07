4a9907508d344c9db59f6aa4d8a37bb0997ff175
Khdscor <khds_cor@naver.com>
2023-07-07 21:01:39 +0900

    refactor: findArticlesService 인터페이스와 여러 구현체들로 분리 테스트

--- backend/footprint/src/test/java/foot/footprint/service/article/FindArticleServiceImplTest.java
+++ backend/footprint/src/test/java/foot/footprint/service/article/FindArticleServiceImplTest.java
@@ -1,15 +1,12 @@
 package foot.footprint.service.article;
 
-import foot.footprint.domain.article.application.FindArticleService;
+import foot.footprint.domain.article.application.findArticle.FindGroupedArticles;
+import foot.footprint.domain.article.application.findArticle.FindPublicAndPrivateArticles;
 import foot.footprint.domain.article.domain.Article;
 import foot.footprint.domain.article.domain.LocationRange;
 import foot.footprint.domain.article.dto.ArticleMapResponse;
 import foot.footprint.domain.article.dao.FindArticleRepository;
 import foot.footprint.domain.article.dto.ArticleRangeRequest;
-import foot.footprint.domain.article.dto.GroupMapArticlesDto;
-import foot.footprint.domain.group.dao.GroupRepository;
-import foot.footprint.global.error.exception.NotExistsException;
-import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,25 +18,24 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
-public class FindArticleServiceTest {
+public class FindArticleServiceImplTest {
 
     @Mock
     private FindArticleRepository findArticleRepository;
 
-    @Mock
-    private GroupRepository groupRepository;
+    @Spy
+    @InjectMocks
+    private FindPublicAndPrivateArticles findPublicAndPrivateArticles;
 
     @Spy
     @InjectMocks
-    private FindArticleService findArticleService;
+    private FindGroupedArticles findGroupedArticles;
 
     @Test
     @DisplayName("전체지도 내 게시글 찾기 테스트")
@@ -52,12 +48,12 @@ public class FindArticleServiceTest {
         given(findArticleRepository.findArticles(userId, locationRange)).willReturn(articles);
 
         //when
-        List<ArticleMapResponse> responses = findArticleService.findPublicMapArticles(
+        List<ArticleMapResponse> responses = findPublicAndPrivateArticles.findArticles(null, null,
             locationRange);
 
         //then
         verify(findArticleRepository, times(1)).findArticles(userId, locationRange);
-        verify(findArticleService, times(1)).findPublicMapArticles(locationRange);
+        verify(findPublicAndPrivateArticles, times(1)).findArticles(null, null, locationRange);
         assertThat(responses).hasSize(1);
     }
 
@@ -65,33 +61,17 @@ public class FindArticleServiceTest {
     @DisplayName("그룹지도")
     public void groupedMapArticlesTest() {
         //given
-        String groupName = "테스트입니다.";
         List<Article> articles = createArticleList(createArticle());
         LocationRange locationRange = new LocationRange(
             new ArticleRangeRequest(10.0, 10.0, 10.0, 10.0));
-        given(groupRepository.findGroupName(any(), any())).willReturn(Optional.of(groupName));
         given(findArticleRepository.findArticlesByGroup(1L, locationRange)).willReturn(articles);
 
         //when
-        GroupMapArticlesDto result = findArticleService.findGroupedArticles(1L, 1L,
+        List<ArticleMapResponse> responses = findGroupedArticles.findArticles(1L, 1L,
             locationRange);
 
         //then
-        assertThat(result.getResponses().size()).isEqualTo(1);
-        assertThat(result.getGroupName()).isEqualTo(groupName);
-    }
-
-    @Test
-    @DisplayName("그룹지도 - 그룹에 가입된 회원이 아닐 시")
-    public void groupedMapArticlesTest_IfNotJoined() {
-        //given
-        LocationRange locationRange = new LocationRange(
-            new ArticleRangeRequest(10.0, 10.0, 10.0, 10.0));
-
-        //when & then
-        assertThatThrownBy(
-            () -> findArticleService.findGroupedArticles(1L, 1L, locationRange)).isInstanceOf(
-            NotExistsException.class);
+        assertThat(responses.size()).isEqualTo(1);
     }
 
     private Article createArticle() {
