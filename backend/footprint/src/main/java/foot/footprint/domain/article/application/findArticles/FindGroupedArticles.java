ac3583767291fb8de86c2cb713ad061663b87021
Khdscor <khds_cor@naver.com>
2023-08-06 16:50:38 +0900

    feat: findArticlesService group을 find할 시 group에 member가 속하는지 검증하는 메서드 구현 및 테스트

--- backend/footprint/src/main/java/foot/footprint/domain/article/application/findArticles/FindGroupedArticles.java
+++ backend/footprint/src/main/java/foot/footprint/domain/article/application/findArticles/FindGroupedArticles.java
@@ -4,6 +4,8 @@ import foot.footprint.domain.article.dao.FindArticleRepository;
 import foot.footprint.domain.article.domain.Article;
 import foot.footprint.domain.article.domain.LocationRange;
 import foot.footprint.domain.article.dto.ArticleMapResponse;
+import foot.footprint.domain.group.dao.MemberGroupRepository;
+import foot.footprint.global.error.exception.NotAuthorizedOrExistException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -17,12 +19,21 @@ public class FindGroupedArticles implements FindArticlesService {
 
     private final FindArticleRepository findArticleRepository;
 
+    private final MemberGroupRepository memberGroupRepository;
+
 
     @Override
     @Transactional(readOnly = true)
     public List<ArticleMapResponse> findArticles(Long memberId, Long groupId,
         LocationRange locationRange) {
+        validateMemberInGroup(memberId, groupId);
         List<Article> articles = findArticleRepository.findArticlesByGroup(groupId, locationRange);
         return ArticleMapResponse.toResponses(articles);
+    }
+
+    private void validateMemberInGroup(Long memberId, Long groupId){
+        if(!memberGroupRepository.existsMemberInGroup(memberId, groupId)){
+            throw new NotAuthorizedOrExistException("해당 그룹에 접근할 권한이 없습니다.");
+        }
     }
 }
