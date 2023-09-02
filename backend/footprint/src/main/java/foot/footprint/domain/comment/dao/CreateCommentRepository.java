a8d233b98095f4e9214b4722c1b08e0ebe422f40
Khdscor <khds_cor@naver.com>
2023-09-02 16:16:26 +0900

    refactor: 게시글 조회 시 댓글은 첫 10개만 id의 역순으로 조회하도록 수정

--- backend/footprint/src/main/java/foot/footprint/domain/comment/dao/CreateCommentRepository.java
+++ backend/footprint/src/main/java/foot/footprint/domain/comment/dao/CreateCommentRepository.java
@@ -1,10 +1,13 @@
 package foot.footprint.domain.comment.dao;
 
 import foot.footprint.domain.comment.domain.Comment;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CreateCommentRepository {
 
     Long saveComment(Comment comment);
+
+    int saveCommentList(List<Comment> comments);
 }
