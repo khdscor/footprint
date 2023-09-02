5846220a532488eb07a277b4fa44915dd51b50c4
Khdscor <khds_cor@naver.com>
2023-09-02 16:37:02 +0900

    feat: 댓글 객체 랜덤 생성하는 easyRandom 메서드 구현

---
+++ backend/footprint/src/test/java/foot/footprint/featureFactory/CommentFeatureFactory.java
@@ -0,0 +1,32 @@
+package foot.footprint.featureFactory;
+
+import static org.jeasy.random.FieldPredicates.inClass;
+import static org.jeasy.random.FieldPredicates.named;
+import static org.jeasy.random.FieldPredicates.ofType;
+
+import foot.footprint.domain.article.domain.Article;
+import foot.footprint.domain.comment.domain.Comment;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.time.LocalDate;
+import java.util.function.Predicate;
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+
+public class CommentFeatureFactory {
+
+    static public EasyRandom create(Long articleId, Long memberId) {
+        Predicate<Field> commentIdPredicate = named("id").and(ofType(Long.class))
+            .and(inClass(Comment.class));
+        Predicate<Field> memberIdPredicate = named("member_id").and(ofType(Long.class))
+            .and(inClass(Comment.class));
+        Predicate<Field> articleIdPredicate = named("article_id").and(ofType(Long.class))
+            .and(inClass(Comment.class));
+        EasyRandomParameters param = new EasyRandomParameters().excludeField(commentIdPredicate)
+            .dateRange(LocalDate.of(2022, 5, 1), LocalDate.of(2023, 7, 22))
+            .stringLengthRange(4, 46)
+            .randomize(articleIdPredicate, () -> articleId)
+            .randomize(memberIdPredicate, () -> memberId);
+        return new EasyRandom(param);
+    }
+}
