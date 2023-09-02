dc30e59ee3454b22718bffb7f54e15d658ac06bc
Khdscor <khds_cor@naver.com>
2023-09-02 17:32:06 +0900

    test: comment easyRandom 객체 생성 시 매개변수에 commentId 추가

--- backend/footprint/src/test/java/foot/footprint/featureFactory/CommentFeatureFactory.java
+++ backend/footprint/src/test/java/foot/footprint/featureFactory/CommentFeatureFactory.java
@@ -4,10 +4,8 @@ import static org.jeasy.random.FieldPredicates.inClass;
 import static org.jeasy.random.FieldPredicates.named;
 import static org.jeasy.random.FieldPredicates.ofType;
 
-import foot.footprint.domain.article.domain.Article;
 import foot.footprint.domain.comment.domain.Comment;
 import java.lang.reflect.Field;
-import java.lang.reflect.Member;
 import java.time.LocalDate;
 import java.util.function.Predicate;
 import org.jeasy.random.EasyRandom;
@@ -15,18 +13,21 @@ import org.jeasy.random.EasyRandomParameters;
 
 public class CommentFeatureFactory {
 
-    static public EasyRandom create(Long articleId, Long memberId) {
+    static public EasyRandom create(Long commentId, Long articleId, Long memberId) {
         Predicate<Field> commentIdPredicate = named("id").and(ofType(Long.class))
             .and(inClass(Comment.class));
         Predicate<Field> memberIdPredicate = named("member_id").and(ofType(Long.class))
             .and(inClass(Comment.class));
         Predicate<Field> articleIdPredicate = named("article_id").and(ofType(Long.class))
             .and(inClass(Comment.class));
-        EasyRandomParameters param = new EasyRandomParameters().excludeField(commentIdPredicate)
+        EasyRandomParameters param = new EasyRandomParameters()
             .dateRange(LocalDate.of(2022, 5, 1), LocalDate.of(2023, 7, 22))
             .stringLengthRange(4, 46)
             .randomize(articleIdPredicate, () -> articleId)
             .randomize(memberIdPredicate, () -> memberId);
+        if(commentId >= 0) {
+            param.excludeField(commentIdPredicate);
+        }
         return new EasyRandom(param);
     }
 }
