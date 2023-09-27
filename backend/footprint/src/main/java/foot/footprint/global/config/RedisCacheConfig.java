0b6e445eb0f5db6ca627227196e361a700ea26cd
Khdscor <khds_cor@naver.com>
2023-09-27 21:47:12 +0900

    feat: redis 캐시 데이터 save, get 유틸 메서드 구현

---
+++ backend/footprint/src/main/java/foot/footprint/global/config/RedisCacheConfig.java
@@ -0,0 +1,23 @@
+package foot.footprint.global.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
---
+++ backend/footprint/src/main/java/foot/footprint/global/util/ObjectSerializer.java
@@ -0,0 +1,43 @@
+package foot.footprint.global.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import foot.footprint.global.error.exception.WrongAccessRedisException;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ObjectSerializer {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public <T> void saveData(String key, T data, int minutes) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String value = mapper.writeValueAsString(data);
+            redisTemplate.opsForValue().set(key, value, minutes, TimeUnit.MINUTES);
+        } catch (Exception e) {
+            log.info("예외 메세지: " + e.getMessage());
+            throw new WrongAccessRedisException("캐시에 데이터를 저장하는데 실패하였습니다.");
+        }
+    }
+
+    public <T> Optional<T> getData(String key, Class<T> classType) {
+        String value = redisTemplate.opsForValue().get(key);
+        if (value == null) {
+            return Optional.empty();
+        }
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return Optional.of(mapper.readValue(value, classType));
+        } catch (Exception e) {
+            log.info("예외 메세지: " + e.getMessage());
+            throw new WrongAccessRedisException("캐시에서 데이터를 가져오는데 실패하였습니다.");
+        }
+    }
+}
