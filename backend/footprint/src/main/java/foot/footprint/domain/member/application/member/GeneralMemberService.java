30c4421d387c8ecdc54a703f6da98e7a90122950
Khdscor <khds_cor@naver.com>
2023-09-27 22:23:49 +0900

    refactor: 맴버 프로필 이미지 조회 기능을 캐시를 도입하여 성능 향상

--- backend/footprint/src/main/java/foot/footprint/domain/member/application/member/GeneralMemberService.java
+++ backend/footprint/src/main/java/foot/footprint/domain/member/application/member/GeneralMemberService.java
@@ -5,22 +5,31 @@ import foot.footprint.domain.member.domain.Member;
 import foot.footprint.domain.member.dto.MemberImageResponse;
 import foot.footprint.domain.member.dto.MyPageResponse;
 import foot.footprint.global.error.exception.NotExistsException;
+import foot.footprint.global.util.ObjectSerializer;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class GeneralMemberService implements MemberService{
+public class GeneralMemberService implements MemberService {
 
     private final MemberRepository memberRepository;
+
+    private final ObjectSerializer objectSerializer;
 
     @Override
     @Transactional(readOnly = true)
     public MemberImageResponse findImageUrl(Long memberId) {
-        Member member = memberRepository.findById(memberId)
-            .orElseThrow(() -> new NotExistsException("존재하지 않는 회원입니다."));
-        return new MemberImageResponse(member.getId(), member.getImage_url());
+        String redisKey = "memberImageUrl::" + memberId;
+        Optional<MemberImageResponse> cacheData = findByRedis(redisKey);
+        if (cacheData.isPresent()) {
+            return cacheData.get();
+        }
+        MemberImageResponse response = findByDB(memberId);
+        objectSerializer.saveData(redisKey, response, 5);
+        return response;
     }
 
     @Override
@@ -33,8 +42,18 @@ public class GeneralMemberService implements MemberService{
     @Transactional
     public void withdraw(Long id) {
         int deleted = memberRepository.deleteMember(id);
-        if(deleted ==0){
+        if (deleted == 0) {
             throw new NotExistsException("존재하지 않는 계정입니다.");
         }
+    }
+
+    private Optional<MemberImageResponse> findByRedis(String redisKey) {
+        return objectSerializer.getData(redisKey, MemberImageResponse.class);
+    }
+
+    private MemberImageResponse findByDB(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new NotExistsException("존재하지 않는 회원입니다."));
+        return new MemberImageResponse(member.getId(), member.getImage_url());
     }
 }
