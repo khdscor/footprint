f229983e2961f0d26ad6d60f7a044cd0a8345e37
Khdscor <khds_cor@naver.com>
2023-12-19 23:47:24 +0900

    refactor: 불필요한 commentLikeRepository#findCommentIdsILiked 메서드 제거

--- backend/footprint/src/test/java/foot/footprint/repository/commentLike/CommentLIkeRepositoryTest.java
+++ backend/footprint/src/test/java/foot/footprint/repository/commentLike/CommentLIkeRepositoryTest.java
@@ -47,42 +47,6 @@ public class CommentLIkeRepositoryTest extends RepositoryTest {
     }
 
     @Test
-    public void findHasILiked() {
-        //given
-        // 2명의 유저
-        Member member = buildMember();
-        memberRepository.saveMember(member);
-        Member another = buildMember();
-        memberRepository.saveMember(another);
-        //게시글 생성
-        Article article = buildArticle(member.getId());
-        createArticleRepository.saveArticle(article);
-        //4개의 댓글, 3개는 member, 1개는 another
-        Comment comment1 = buildComment(member.getId(), article.getId());
-        createCommentRepository.saveComment(comment1);
-        Comment comment2 = buildComment(member.getId(), article.getId());
-        createCommentRepository.saveComment(comment2);
-        Comment comment3 = buildComment(member.getId(), article.getId());
-        createCommentRepository.saveComment(comment3);
-        Comment comment4 = buildComment(another.getId(), article.getId());
-        createCommentRepository.saveComment(comment4);
-        //3개의 좋아요, 2개는 member 1개는 another
-        CommentLike commentLike1 = buildCommentLike(comment1.getId(), member.getId());
-        commentLikeRepository.saveCommentLike(commentLike1);
-        CommentLike commentLike2 = buildCommentLike(comment2.getId(), another.getId());
-        commentLikeRepository.saveCommentLike(commentLike2);
-        CommentLike commentLike4 = buildCommentLike(comment4.getId(), member.getId());
-        commentLikeRepository.saveCommentLike(commentLike4);
-
-        //when
-        List<Long> commentLikes = commentLikeRepository.findCommentIdsILiked(article.getId(),
-            member.getId());
-
-        //then
-        assertThat(commentLikes).hasSize(2);
-    }
-
-    @Test
     public void deleteCommentLike() {
         //given
         Member member = buildMember();
