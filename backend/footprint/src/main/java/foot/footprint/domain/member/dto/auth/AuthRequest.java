e9170612937bcf683bf296f71b65c55e5a7a2497
Khdscor <khds_cor@naver.com>
2024-02-10 22:28:18 +0900

    feat: 이메일 형식이 다를 시 예외 처리 추가

--- backend/footprint/src/main/java/foot/footprint/domain/member/dto/auth/AuthRequest.java
+++ backend/footprint/src/main/java/foot/footprint/domain/member/dto/auth/AuthRequest.java
@@ -1,8 +1,8 @@
 package foot.footprint.domain.member.dto.auth;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,11 +13,12 @@ public class AuthRequest {
 
     @NotBlank(message = "이메일을 입력해주세요")
     @NotEmpty(message = "이메일을 입력해주세요")
-    @Size(min = 4, max = 30)
+    @Size(min = 4, max = 30, message = "이메일은 4~30자 사이로 입력해주세요")
+    @Pattern(regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
     @NotBlank(message = "비밀번호를 입력해주세요")
     @NotEmpty(message = "비밀번호를 입력해주세요")
-    @Size(min = 4, max = 30)
+    @Size(min = 4, max = 30, message = "비밀번호는 4~10자 사이로 입력해주세요")
     private String password;
 }
